761b7b7c91aad25f0da608ab04af7113e54e698a
Jess Pates <jesspates@hotmail.co.uk>
2023-09-09 22:39:57 +0100

    hero text positioning

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1440px",
+    },
     extend: {
       colors: {
         dark: '#1C1C1C',
